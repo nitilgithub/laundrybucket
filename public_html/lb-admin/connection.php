1fbeb22ab3da1e0feed3d1a621a32249b9c1e412
nitilgithub <34202180+nitilgithub@users.noreply.github.com>
2017-12-18 17:35:18 +1300

    Update connection.php
    Changes in DB configuration

--- public_html/lb-admin/connection.php
+++ public_html/lb-admin/connection.php
@@ -1,6 +1,6 @@
 <?php
-$conn=mysql_connect("132.148.21.135","laundryb_lusr","x~[i#[96J;Sq") or die(mysql_error());
-$db=mysql_select_db("laundryb_ldb",$conn) or die(mysql_error());
+$conn=mysql_connect("sangwanbrothersproductiondb.clp1di3hdfiu.ap-south-1.rds.amazonaws.com","radmin","Ouat1agffA#") or die(mysql_error());
+$db=mysql_select_db("laundrydb",$conn) or die(mysql_error());
 //$conn=mysql_connect("localhost","laundry_user","Ouat1agffA") or die(mysql_error());
 //$db=mysql_select_db("laundry_db",$conn) or die(mysql_error());
 ?>
